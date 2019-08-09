0e839ca49de5141d5baac731bc3b75bcad8fa79b
Syzygianinfern0 <spsharan2000@gmail.com>
2019-08-10 02:27:11 +0530

    VGG19 Download and Try

--- basic.py
+++ basic.py
@@ -15,8 +15,15 @@ def read_images(path1, path2):
 
 if __name__ == '__main__':
     content_img, style_img = read_images('assets/content_images/nitt.jpg', 'assets/style_images/starry.jpg')
-    cv2.imshow('content_img', content_img)
-    cv2.imshow('style_img', style_img)
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.imshow('content_img', content_img)
+    # cv2.imshow('style_img', style_img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    vgg = tf.keras.applications.VGG19(include_top=True, weights='imagenet')     # The VGG19 Model
+    img = tf.keras.applications.vgg19.preprocess_input(content_img)             # Normalization
+    img = tf.image.resize(img, (224, 224))       # Input for VGG19 must be (None,224,224,3)
+    prediction_probabilities = vgg(tf.expand_dims(img, axis=0))
+    predicted_top_5 = tf.keras.applications.vgg19.decode_predictions(prediction_probabilities.numpy())[0]
+    print([(class_name, prob) for (number, class_name, prob) in predicted_top_5])

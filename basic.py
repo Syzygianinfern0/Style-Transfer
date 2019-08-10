5dc50a10a70d98539d0bba794dc89d5a258faa91
Syzygianinfern0 <spsharan2000@gmail.com>
2019-08-10 12:27:47 +0530

    VGG maker

--- basic.py
+++ basic.py
@@ -30,8 +30,25 @@ def gram_matrix(input_tensor: tf.Tensor):
     # it is equivalent to : np.dot(a[0][0].T,a[0][0]) + np.dot(a[0][1].T, a[0][1])
     # if 'a' is of shape (1,2,3,4)
     result = tf.linalg.einsum('bijc,bijd->bcd', input_tensor, input_tensor)
-    return result / tf.shape(input_tensor.shape[1] * input_tensor.shape[2])     # Normalize
+    return result / tf.shape(input_tensor.shape[1] * input_tensor.shape[2])  # Normalize
+
+
+# noinspection PyShadowingNames
+def get_model(content, style):
+    # Loading the Awesome VGG. Set weights to a constant to avoid changing them
+    vgg = tf.keras.applications.vgg19.VGG19(include_top=False, weights='imagenet')
+    vgg.trainable = False
+    layers = content + style
+    out = [vgg.get_layer(layer).output for layer in layers]
+    return tf.keras.Model(vgg.input, out)
 
 
 if __name__ == '__main__':
+    content = ['block5_conv2']  # From the research paper, these were the best layers to work with
+    style = ['block1_conv1',
+             'block2_conv1',
+             'block3_conv1',
+             'block4_conv1',
+             'block5_conv1']
+
     content_img, style_img = read_images('assets/content_images/nitt.jpg', 'assets/style_images/starry.jpg')

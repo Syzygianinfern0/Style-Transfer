26870c7570f8577b1f99610eb4b9136bd7a09401
Syzygianinfern0 <spsharan2000@gmail.com>
2019-08-10 12:13:40 +0530

    Gram Matrix Added

--- basic.py
+++ basic.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 from time import sleep
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 def read_images(path1, path2):
@@ -13,77 +16,22 @@ def read_images(path1, path2):
     return img1, img2
 
 
+def preprocess(image):
+    image = tf.keras.applications.vgg19.preprocess_input(image)
+    return image
+
+
+def gram_matrix(input_tensor: tf.Tensor):
+    # b - Batch size
+    # i - Rows
+    # j - Cols
+    # c (d) - Channels (letters cannot be reused so we use d)
+    # We need to reduce_sum across the i'th dimension and multiply the transpose and itself in the next 2 dimensions
+    # it is equivalent to : np.dot(a[0][0].T,a[0][0]) + np.dot(a[0][1].T, a[0][1])
+    # if 'a' is of shape (1,2,3,4)
+    result = tf.linalg.einsum('bijc,bijd->bcd', input_tensor, input_tensor)
+    return result / tf.shape(input_tensor.shape[1] * input_tensor.shape[2])     # Normalize
+
+
 if __name__ == '__main__':
     content_img, style_img = read_images('assets/content_images/nitt.jpg', 'assets/style_images/starry.jpg')
-
-    # cv2.imshow('content_img', content_img)
-    # cv2.imshow('style_img', style_img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
-    vgg = tf.keras.applications.VGG19(include_top=True, weights='imagenet')  # The VGG19 Model
-    print(vgg.summary())
-    """
-    Layer (type)                 Output Shape              Param #   
-=================================================================
-input_1 (InputLayer)         [(None, 224, 224, 3)]     0         
-_________________________________________________________________
-block1_conv1 (Conv2D)        (None, 224, 224, 64)      1792      
-_________________________________________________________________
-block1_conv2 (Conv2D)        (None, 224, 224, 64)      36928     
-_________________________________________________________________
-block1_pool (MaxPooling2D)   (None, 112, 112, 64)      0         
-_________________________________________________________________
-block2_conv1 (Conv2D)        (None, 112, 112, 128)     73856     
-_________________________________________________________________
-block2_conv2 (Conv2D)        (None, 112, 112, 128)     147584    
-_________________________________________________________________
-block2_pool (MaxPooling2D)   (None, 56, 56, 128)       0         
-_________________________________________________________________
-block3_conv1 (Conv2D)        (None, 56, 56, 256)       295168    
-_________________________________________________________________
-block3_conv2 (Conv2D)        (None, 56, 56, 256)       590080    
-_________________________________________________________________
-block3_conv3 (Conv2D)        (None, 56, 56, 256)       590080    
-_________________________________________________________________
-block3_conv4 (Conv2D)        (None, 56, 56, 256)       590080    
-_________________________________________________________________
-block3_pool (MaxPooling2D)   (None, 28, 28, 256)       0         
-_________________________________________________________________
-block4_conv1 (Conv2D)        (None, 28, 28, 512)       1180160   
-_________________________________________________________________
-block4_conv2 (Conv2D)        (None, 28, 28, 512)       2359808   
-_________________________________________________________________
-block4_conv3 (Conv2D)        (None, 28, 28, 512)       2359808   
-_________________________________________________________________
-block4_conv4 (Conv2D)        (None, 28, 28, 512)       2359808   
-_________________________________________________________________
-block4_pool (MaxPooling2D)   (None, 14, 14, 512)       0         
-_________________________________________________________________
-block5_conv1 (Conv2D)        (None, 14, 14, 512)       2359808   
-_________________________________________________________________
-block5_conv2 (Conv2D)        (None, 14, 14, 512)       2359808   
-_________________________________________________________________
-block5_conv3 (Conv2D)        (None, 14, 14, 512)       2359808   
-_________________________________________________________________
-block5_conv4 (Conv2D)        (None, 14, 14, 512)       2359808   
-_________________________________________________________________
-block5_pool (MaxPooling2D)   (None, 7, 7, 512)         0         
-_________________________________________________________________
-flatten (Flatten)            (None, 25088)             0         
-_________________________________________________________________
-fc1 (Dense)                  (None, 4096)              102764544 
-_________________________________________________________________
-fc2 (Dense)                  (None, 4096)              16781312  
-_________________________________________________________________
-predictions (Dense)          (None, 1000)              4097000   
-=================================================================
-Total params: 143,667,240
-Trainable params: 143,667,240
-Non-trainable params: 0
-_________________________________________________________________"""
-    # img = tf.keras.applications.vgg19.preprocess_input(content_img)             # Normalization
-    # img = tf.image.resize(img, (224, 224))       # Input for VGG19 must be (None,224,224,3)
-    # prediction_probabilities = vgg(tf.expand_dims(img, axis=0))
-    # predicted_top_5 = tf.keras.applications.vgg19.decode_predictions(prediction_probabilities.numpy())[0]
-    # print([(class_name, prob) for (number, class_name, prob) in predicted_top_5])
